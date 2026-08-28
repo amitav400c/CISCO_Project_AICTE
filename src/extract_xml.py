d9bac90f1412306f8c7b8e99b5d64df351e36103
Anushka Banerjee <2304011@kiit.ac.in>
2026-08-28 12:12:50 +0530

    Fix proper Unpacket submodule link

--- src/extract_xml.py
+++ src/extract_xml.py
@@ -6,31 +6,27 @@ import xml.etree.ElementTree as ET
 root = Path(__file__).resolve().parent.parent
 sub_path = root / "Unpacket"
 #Add recurive path import for the files inside sub_path
-sys.path.append(str(sub_path))
+sys.path.insert(0, str(sub_path))
 
-from Decipher.pt_crypto import decrypt_pkt
-def extract_xml(input_files: Path, output_files: Path):
-    output_files.mkdir(parents=True, exist_ok=True)
-    for ptf in input_files.glob("*.pkt"):
-        print(f"Processing: {ptf}")
 
-        try :
-            with open(ptf, "rb") as f:
-                data = f.read()
-                xml_data = decrypt_pkt(data)
+def extract_xml(input_files: Path, output_files: Path) -> None:
+	from Decipher.pt_crypto import decrypt_pkt
 
-            root = ET.fromstring(xml_data)
-            tree = ET.ElementTree(root)
+	output_files.mkdir(parents=True, exist_ok=True)
+	for packet_file in input_files.glob("*.pkt"):
+		print(f"Processing: {packet_file}")
 
-            if hasattr(ET, "indent"):
-                ET.indent(tree, space="  ")
-                # test.pkt -> test.xml
-                output_file = output_files / ptf.with_suffix(".xml").name
-                tree.write(
-                    output_file,
-                    encoding="utf-8",
-                    xml_declaration=True
-                )
-                print(f"[+] Created {output_file.name}")
-        except Exception as e:
-            print(f"[-] Failed: {ptf.name}: {e}")
+		try:
+			with packet_file.open("rb") as file:
+				xml_data = decrypt_pkt(file.read())
+
+			xml_root = ET.fromstring(xml_data)
+			tree = ET.ElementTree(xml_root)
+			if hasattr(ET, "indent"):
+				ET.indent(tree, space="  ")
+
+			output_file = output_files / packet_file.with_suffix(".xml").name
+			tree.write(output_file, encoding="utf-8", xml_declaration=True)
+			print(f"[+] Created {output_file.name}")
+		except Exception as error:
+			print(f"[-] Failed: {packet_file.name}: {error}")
